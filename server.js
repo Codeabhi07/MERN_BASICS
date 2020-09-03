a10cb84236bbb0a0271f9c5cfa72d0d5f6ad569c
Abhishek Jain <jainabhishek7765@gmail.com>
2020-09-03 12:43:32 +0530

    Sending response using Express

---
+++ server.js
@@ -0,0 +1,10 @@
+var express= require('express');
+var app= express();
+
+app.get('/',(req,res)=> {
+    res.send("Hello World");
+});
+
+app.listen(8000, (err) =>{
+    console.log("Successful",err);
+});
